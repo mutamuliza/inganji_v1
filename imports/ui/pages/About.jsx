02a5f07d0d16c869c790dba2acb88ab0c94c144c
Mucyowicyogere <arnaudmucyo3@gmail.com>
2018-07-31 15:51:59 +0200

    some modifications on pages

--- imports/ui/pages/About.jsx
+++ imports/ui/pages/About.jsx
@@ -2,9 +2,163 @@ import React from 'react';
 
 function About() {
   return (
-    <div className='About'style={{color:"#3b97ff"}}>
-      <h1>About</h1>
+    <div className='About' style={{backgroundColor:"#001129", color:"white"}} >
+ 
+                <h1>About us</h1>
+                <p>Tugende is a software used for booking and buying tickets all across the country. It is designed especially for professionals, students in Universities and high schools, public servants, private workers, business people, and other passengers who want to ride intercity. We provide accurate information about the available buses depending on the agency that the customer wants to use.</p>
+                
+                <h1> Our Mission</h1>
+                <p>Tugende's mission is "to be the bridge between the customers and the bus operators in the country." Ensuring that the customers receive accurate and reliable information is what we want in order to raise awareness among the citizens. Our vision is to be the leading technology company with strong partnership within Africa.</p>
+                <h3><span>"With Tugende app get your ticket now in an eye blink"</span></h3>
+                
+                <h1>Our Team</h1>
+                <div> <strong>DIVINE MARTINE UMUHIRE</strong><br/>
+                <img src="" alt="Umuhire's profile"  />
+               <p>Team's hustler<br/>
+                interested in learning new things</p> </div>
+                
+                <div><strong>KANYANKORE MUCYO Arnaud</strong>
+                <img src="" alt="Mucyo's profile" />
+                <p>Team's Hacker<br/>
+                interested in traditional culture</p></div>
+                
+                <div><strong>MUTAMURIZA Jennifer</strong><br/>
+                <img src="" alt="Mutamuliza's profile" />
+                <p>Team's Hustler<br/>
+                interested in inspiring young women</p>
+                </div>
+                
+               <div><strong>KUBWIMANA Patrick</strong>
+               <img src="" alt="Kubwimana's profile" />
+                <p>Team's Hipster<br/>
+                passionate about modern technology and high tech designs with an experience on smart cities.</p>
+                 </div>
+                 <footer className="page-footer font-small mdb-color lighten-3 pt-4">
+
+    
+    <div className="container text-center text-md-left">
+
+   
+      <div className="row">
+
      
+        <div className="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+         
+          <h5 className="font-weight-bold text-uppercase mb-4">Footer Content</h5>
+          <p>Here you can use rows and columns here to organize your footer content.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate esse
+            quasi, veritatis totam voluptas nostrum.</p>
+
+        </div>
+       
+
+        <hr className="clearfix w-100 d-md-none"/>
+
+       
+        <div className="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        
+          <h5 className="font-weight-bold text-uppercase mb-4">About</h5>
+
+          <ul className="list-unstyled">
+            <li>
+              <p>
+                <a href="#!">PROJECTS</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="#!">ABOUT US</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="#!">BLOG</a>
+              </p>
+            </li>
+            <li>
+              <p>
+                <a href="#!">AWARDS</a>
+              </p>
+            </li>
+          </ul>
+
+        </div>
+     
+
+        <hr className="clearfix w-100 d-md-none"/>
+
+       
+        <div className="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+         
+          <h5 className="font-weight-bold text-uppercase mb-4">Address</h5>
+
+          <ul className="list-unstyled">
+            <li>
+              <p>
+                <i className="fa fa-home mr-3"></i> New York, NY 10012, US</p>
+            </li>
+            <li>
+              <p>
+                <i className="fa fa-envelope mr-3"></i> info@example.com</p>
+            </li>
+            <li>
+              <p>
+                <i className="fa fa-phone mr-3"></i> + 01 234 567 88</p>
+            </li>
+            <li>
+              <p>
+                <i className="fa fa-print mr-3"></i> + 01 234 567 89</p>
+            </li>
+          </ul>
+
+        </div>
+       
+
+        <hr className="clearfix w-100 d-md-none"/>
+
+       
+        <div className="col-md-2 col-lg-2 text-center mx-auto my-4">
+
+        
+          <h5 className="font-weight-bold text-uppercase mb-4">Follow Us</h5>
+
+         
+          <a type="button" className="btn-floating btn-fb">
+            <i class="fa fa-facebook"></i>
+          </a>
+          
+          <a type="button" className="btn-floating btn-tw">
+            <i class="fa fa-twitter"></i>
+          </a>
+       
+          <a type="button" className="btn-floating btn-gplus">
+            <i class="fa fa-google-plus"></i>
+          </a>
+          
+          <a type="button" className="btn-floating btn-dribbble">
+            <i class="fa fa-dribbble"></i>
+          </a>
+
+        </div>
+       
+      
+    
+    
+
+   
+    <div class="footer-copyright text-center py-3">© 2018 Copyright:
+      <a href="https://mdbootstrap.com/bootstrap-tutorial/"> tugende.com</a>
+    </div>
+     </div>
+      
+
+    </div>
+
+  </footer>
+                
     </div>
   );
 }
